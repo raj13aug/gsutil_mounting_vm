1cf6c9d7eb62527f63311dee27f4659d61bde5e2
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-04-24 21:55:31 +0530

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s` echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update -y && sudo apt-get install gcsfuse -y
-mkdir /home/gcp/gcs-bucket  // create mount path for our bucket
+mkdir /home/gcp/gcs-bucket  
 sudo chown -R 777 /home/gcp/gcs-bucket
 sudo gcsfuse -o allow_other -file-mode=777 -dir-mode=777 model-artifact-bucket /home/gcp/gcs-bucket
